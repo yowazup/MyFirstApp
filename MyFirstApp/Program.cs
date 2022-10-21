7caab5cf7c2b549378885d62aa40b870cc255a87
Nikita <nikita.zakharov@gmail.com>
2022-10-22 01:37:13 +0300

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string myName;
+        myName = "Nikita";
+
+        Console.WriteLine(myName);
+        Console.WriteLine("\t \t Привет, Мир");
+        Console.WriteLine("\t \t Мне 35 лет");
+        Console.WriteLine("\t \t My name is \n {0}", myName);
+        Console.WriteLine("\u0023");
+
+        Console.ReadKey();
+
+    }
+}
