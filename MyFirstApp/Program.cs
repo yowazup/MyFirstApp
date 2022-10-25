de4004e481a9207b836f7659f9886f7647ef4727
Nikita <nikita.zakharov@gmail.com>
2022-10-25 12:05:47 +0300

    added some descriptions

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -7,9 +7,11 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        //Ввод имени
         Console.Write("\n Enter your name: ");
         var name = Console.ReadLine();
 
+        //Ввод даты рождения и преодбразование в формат даты
         Console.Write("\n Enter your birthdate: ");
         string inputdate = Console.ReadLine();
         int year = int.Parse(inputdate.Substring(6,4));
@@ -17,10 +19,13 @@ class MainClass
         int day = int.Parse(inputdate.Substring(0, 2));
         var birthdate = new DateTime (year, month, day);
 
+        //Расчет возраста
         DateTime today = DateTime.Today;        
         TimeSpan diff = today - birthdate;
         double longage = diff.TotalDays / 365.25;
         int age = (int) longage;
+        
+        //Вывод ответа
         Console.WriteLine("\n Hello, {0}. Today is {1} and your age is {2} years.", name, today.ToString("dd MMMM yyyy"), age);
 
         Console.ReadKey();
