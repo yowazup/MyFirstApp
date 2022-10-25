28c3d57220c677d274ef3c7be7c52cc7c5dbcbd0
Nikita <nikita.zakharov@gmail.com>
2022-10-25 11:58:39 +0300

    lesson 3.7

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,21 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
-        string myName = "Niktia";
-        byte Age = 27;
-        bool HaveIApet = true;
-        double MyShoeSize = 42.5;
+        Console.Write("\n Enter your name: ");
+        var name = Console.ReadLine();
 
+        Console.Write("\n Enter your birthdate: ");
+        string inputdate = Console.ReadLine();
+        int year = int.Parse(inputdate.Substring(6,4));
+        int month = int.Parse(inputdate.Substring(3, 2));
+        int day = int.Parse(inputdate.Substring(0, 2));
+        var birthdate = new DateTime (year, month, day);
 
-        Console.WriteLine("My name is " + myName);
-        Console.WriteLine("My age is " + Age);
-        Console.WriteLine("Do I have a pet " + HaveIApet);
-        Console.WriteLine("My shoe size " + MyShoeSize);
+        DateTime today = DateTime.Today;        
+        TimeSpan diff = today - birthdate;
+        double longage = diff.TotalDays / 365.25;
+        int age = (int) longage;
+        Console.WriteLine("\n Hello, {0}. Today is {1} and your age is {2} years.", name, today.ToString("dd MMMM yyyy"), age);
 
         Console.ReadKey();
 
