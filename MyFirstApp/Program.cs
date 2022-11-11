24be4577f2f2fb82281df67453e1369d4549c3e1
Nikita <nikit.zakharov@gmail.com>
2022-11-12 00:03:45 +0300

    Задание 5.5.3

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -3,40 +3,50 @@ using System.Globalization;
 
 class MainClass
 {
-    public static void Main(string[] args)
+
+    static void Main(string[] args)
     {
+        Console.WriteLine("Напишите что-то");
+        var str = Console.ReadLine();
 
-    (string Name, string LastName, string Login, int LoginLength, bool HasPet, double Age, string[] favcolors) User;
+        Console.WriteLine("Укажите глубину эха");
+        var deep = int.Parse(Console.ReadLine());
 
+        Console.WriteLine(str);
 
-        for (int a = 0; a < 3; a++)
+        Echo(str, deep);
 
+        Console.ReadKey();
+    }
+
+    static void Echo(string saidword, int deep)
+    {
+        var modif = saidword;
+
+        if (modif.Length >= 2)
         {
-            Console.WriteLine("Введите имя: ");
-            User.Name = Console.ReadLine();
-
-            Console.WriteLine("Введите фамилию: ");
-            User.LastName = Console.ReadLine();
-
-            Console.WriteLine("Введите логин: ");
-            User.Login = Console.ReadLine();
-            User.LoginLength = User.Login.Length;
-
-            Console.WriteLine("Есть ли у вас животные? Да/Нет.");
-            if (Console.ReadLine() == "Да")
-                User.HasPet = true;
-            else
-                User.HasPet = false;
-
-            Console.WriteLine("Введите возраст пользователя: ");
-            User.Age = Convert.ToDouble(Console.ReadLine());
-
-            User.favcolors = new string[3];
-            Console.WriteLine("Введите три любимых цвета пользователя: ");
-            for (int i = 0; i < User.favcolors.Length; i++)
-            {
-                User.favcolors[i] = Console.ReadLine();
-            }
+            modif = modif.Remove(0, 2);
         }
+
+        if (modif.Length < 2)
+        {
+            Console.WriteLine("win-win-win-win");
+            
+            return;
+        }
+        
+        if (deep >= 15)
+        {
+            deep = 14;
+        }
+
+        Console.BackgroundColor = (ConsoleColor)deep;
+        Console.WriteLine("..." + modif);
+
+        if (deep > 1)
+        {
+            Echo(modif, deep - 1);
+        }
+
     }
 }
